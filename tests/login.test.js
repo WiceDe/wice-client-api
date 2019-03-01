3669e4ec5c32aaa832f1fe85c9c2d073c1242271
Shterion Yanev <shterion.yanev@yahoo.com>
2019-03-01 23:38:34 +0100

    Add user login test to a stage of getting the cookie from server

--- tests/login.test.js
+++ tests/login.test.js
@@ -23,3 +23,21 @@ test('if the header has the correct subtitle', async () => {
   const desc = await page.$eval('.renderedMarkdown > p', el => el.innerHTML);
   expect(desc).toEqual('This is a client API for Wice CRM');
 });
+
+test('user login functionality', async () => {
+  const credentials = '{"mandant_name": "sandbox", "username": "syanev", "password": "d36adb53"}';
+  await page.click('.opblock-summary.opblock-summary-post');
+  await page.click('.try-out__btn');
+  await page.waitForSelector('.body-param__text');
+  await page.focus('.body-param__text');
+  await page.$eval('.body-param__text', el => el.value = '');
+  await page.type('.body-param__text', credentials);
+  await page.click('.opblock-control__btn');
+
+  const element = await page.waitForSelector('.microlight');
+  const response = await page.evaluate(el => el.textContent, element);
+  const jsonResponse = JSON.parse(response);
+  const { cookie } = jsonResponse;
+  console.log(cookie);
+  expect(response).toContain('cookie');
+});
