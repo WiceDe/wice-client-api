6955b5e79dcb1fcd51e55115d96f1ab869ecef18
Shterion <shterion.yanev@yahoo.com>
2019-03-04 13:55:01 +0100

    Check if response is empty

--- api/controllers/person.js
+++ api/controllers/person.js
@@ -33,6 +33,9 @@ router.get('/', async (req, res) => {
       res.status(response.statusCode).send(response.body);
     } else {
       // TODO: Check if array is empty
+      if (response.body.loop_addresses.length === 0) {
+        return res.status(response.statusCode).send('No persons found!');
+      }
 
       response.body.loop_addresses.filter((person) => {
         const currentPerson = customPerson(person);
