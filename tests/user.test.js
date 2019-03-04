447c5bd8cde31aa6c60783b22fe5b4f580a570b5
Shterion <shterion.yanev@yahoo.com>
2019-03-04 10:13:59 +0100

    Add a test for swagger

---
+++ tests/user.test.js
@@ -0,0 +1,34 @@
+/* eslint-disable no-unused-expressions */
+/* eslint no-underscore-dangle: "off" */
+
+process.env.NODE_ENV = 'test';
+
+// const nock = require('nock');
+const chai = require('chai');
+const { expect } = require('chai');
+const chaiHttp = require('chai-http');
+const request = require('supertest');
+const server = require('../index.js');
+const log = require('../api/utils/logger');
+
+// const {
+//   loginUser,
+// } = require('./seed/user.seed');
+
+chai.use(chaiHttp);
+
+describe('/ - Wice CRM API Documentation', () => {
+  it('should display the swagger-generated HTML page', (done) => {
+    request(server)
+      .get('/')
+      .then((res) => {
+        expect(res.text).to.not.be.empty;
+        expect(res.text).to.contain('HTML for static distribution bundle build');
+      })
+      .catch((err) => {
+        log.debug(err);
+        throw err;
+      })
+      .then(done());
+  });
+});
