817faf7d77da394fb386311592cf622cc22f4f40
Shterion <shterion.yanev@yahoo.com>
2019-03-11 14:15:58 +0100

    Create a middleware for verifying the server

---
+++ api/utils/verifyServer.js
@@ -0,0 +1,7 @@
+module.exports = {
+  verifyServer(req, res, next) {
+    const host = req.headers['x-wice-server'];
+    req.server = `https://${host}/plugin/wp_wice_client_api_backend/json`;
+    next();
+  },
+};
