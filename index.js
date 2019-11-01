a30f8a8c6b3b3c09abf0a8fc7867c839d4400517
Shterion <shterion.yanev@yahoo.com>
2019-11-01 16:07:21 +0100

    Refactor index and clean up

--- index.js
+++ index.js
@@ -1,19 +1,60 @@
-const Server = require('./server');
+process.env.NODE_CONFIG_DIR = './config';
+
+const express = require('express');
+const swaggerUi = require('swagger-ui-express');
+const bodyParser = require('body-parser');
+const swaggerDocument = require('./api/swagger/swagger.json');
 const log = require('./api/utils/logger');
 
-const server = new Server();
+const user = require('./api/controllers/user');
+const person = require('./api/controllers/person');
+const organization = require('./api/controllers/organization');
+const article = require('./api/controllers/article');
 
-(() => {
-  try {
-    server.setupRoutes();
-    server.setupSwagger();
+app = express();
+app.use(bodyParser.json());
+app.disable('x-powered-by');
 
-    if (!module.parent) {
-      server.listen(process.env.PORT || 5000);
-    } else {
-      module.exports = server.listen();
-    }
-  } catch (err) {
-    log.error(err);
+const port = process.env.PORT || 5000;
+
+// This middleware insures we always have security headers
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, Authorization, X-Requested-With, Content-Type, Accept, Options',
+  );
+  res.append('Strict-Transport-Security', 'max-age=3600');
+  res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
   }
-})();
+  res.append('Content-Security-Policy', "frame-ancestors 'none'");
+  next();
+});
+
+app.get('/healthcheck', (req, res) => {
+  res.send({
+    status: 'pass',
+    version: '0.0.1',
+    description: 'Wice CRM Client API',
+  });
+});
+
+app.use('/api/v1/users', user);
+app.use('/api/v1/persons', person);
+app.use('/api/v1/organizations', organization);
+app.use('/api/v1/articles', article);
+
+log.info('Routes set.');
+
+app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+if (process.env.NODE_ENV !== 'test') {
+  app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
+}
+
+
+module.exports = app;
