559eb6520e22e6891bf41eb1fc225fe4253ec024
Shterion Yanev <shterion.yanev@yahoo.com>
2019-11-03 15:49:36 +0100

    Add article seeds

--- tests/seed/articles.seed.js
+++ tests/seed/articles.seed.js
@@ -1,37 +1,18 @@
 const nock = require('nock');
 
 const getArticles = nock('https://demo.wice-net.de/plugin/wp_wice_client_api_backend/json?method=get_all_articles&full_list=1&cookie=asdfghjkl')
-  // .persist()
   .post('')
   .reply(200, {
     loop_articles: [{
-      rowid: '873539',
-      mandant: 'sandbox',
-      name: 'Company Ltd.',
-      email: 'info@company.com',
-      street: 'Main Str.',
-      street_number: '441',
-      zip_code: '10016',
-      p_o_box: 'B41C',
-      town: 'New York City',
-      state: 'New York',
-      country: 'USA',
-      phone: '82152587588',
-      fax: '82759871528',
+      rowid: '5759',
+      number: '5759',
+      description: 'LAN Cabel',
+      in_stock: '0',
     }, {
-      rowid: '821528',
-      mandant: 'sandbox',
-      name: 'Organization Ltd.',
-      email: 'info@organization.com',
-      street: 'Long Beach Str.',
-      street_number: '41',
-      zip_code: '94088',
-      p_o_box: 'B41C',
-      town: 'San Jose',
-      state: 'California',
-      country: 'USA',
-      phone: '82152587588',
-      fax: '82759871528',
+      rowid: '5931',
+      number: '5931',
+      description: 'WLAN-Router',
+      in_stock: '100',
     }],
   });
 
@@ -39,9 +20,11 @@ const createArticle = nock('https://demo.wice-net.de/plugin/wp_wice_client_api_b
   .persist()
   .post('')
   .reply(200, {
-    name: 'Company Ltd.',
-    email: 'info@company.com',
+    rowid: '5821',
+    number: '5821',
+    description: 'Monitor Acer 27',
+    in_stock: '50',
   });
 
 
-module.exports = { createArticle, getArticles };
+module.exports = { getArticles, createArticle };
